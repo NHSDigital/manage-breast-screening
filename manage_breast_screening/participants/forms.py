63a1d9ffcd777335170d8fb672d8a7d802274815
Mat Moore <MatMoore@users.noreply.github.com>
2025-07-01 17:18:20 +0100

    Implement form to record a past mammogram
    I've used ModelForm here as the data will be saved to a single model,
    and the default behaviour ensures both model-level and form-level
    validation is run and handled by form.errors.
    
    There is some additional customisation in the form's clean() and save()
    to handle fields that are conditionally shown and required.

--- manage_breast_screening/participants/forms.py
+++ manage_breast_screening/participants/forms.py
@@ -1,6 +1,9 @@
-from django import forms
+from enum import StrEnum
 
-from .models import Ethnicity
+from django import forms
+from django.forms import ChoiceField, ModelForm, ValidationError
+
+from .models import Ethnicity, ParticipantReportedMammogram
 
 
 class EthnicityForm(forms.Form):
@@ -38,3 +41,143 @@ class EthnicityForm(forms.Form):
             "ethnic_background_choice"
         ]
         self.participant.save()
+
+
+class ParticipantRecordedMammogramForm(ModelForm):
+    class Meta:
+        model = ParticipantReportedMammogram
+        fields = [
+            "provider",
+            "location_details",
+            "exact_date",
+            "approx_date",
+            "different_name",
+            "additional_information",
+        ]
+
+    class WhereTaken(StrEnum):
+        SAME_UNIT = "same_unit"
+        ANOTHER_UNIT = "another_unit"
+        ELSEWHERE_UK = ParticipantReportedMammogram.LocationType.ELSEWHERE_UK.value
+        OUTSIDE_UK = ParticipantReportedMammogram.LocationType.OUTSIDE_UK.value
+        PREFER_NOT_TO_SAY = (
+            ParticipantReportedMammogram.LocationType.PREFER_NOT_TO_SAY.value
+        )
+
+    def __init__(self, participant, current_provider, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.participant = participant
+        self.current_provider = current_provider
+
+        # Add additional fields which are used on the form for progressively disclosing
+        # other form fields
+        self.fields["where_taken"] = ChoiceField(
+            choices=(
+                (self.WhereTaken.SAME_UNIT, f"At {current_provider.name}"),
+                (
+                    self.WhereTaken.ANOTHER_UNIT,
+                    "At another NHS breast screening unit",
+                ),
+                (self.WhereTaken.ELSEWHERE_UK, "Somewhere else in the UK"),
+                (self.WhereTaken.OUTSIDE_UK, "Outside the UK"),
+                (self.WhereTaken.PREFER_NOT_TO_SAY, "Prefer not to say"),
+            )
+        )
+
+        self.fields["date_accuracy"] = ChoiceField(
+            choices=(
+                ("exact", "Enter an exact date"),
+                ("approx", "Enter an approximate date"),
+            )
+        )
+
+        self.fields["name_is_the_same"] = ChoiceField(
+            choices=(
+                ("yes", "Yes"),
+                ("no", "Enter the previously used name"),
+            )
+        )
+
+        # Explicitly order the films so that the error summary order
+        # matches the order fields are rendered in.
+        self.order_fields(
+            [
+                "where_taken",
+                "provider",
+                "location_details",
+                "date_accuracy",
+                "exact_date",
+                "approx_date",
+                "name_is_the_same",
+                "different_name",
+                "additional_information",
+            ]
+        )
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        where_taken = cleaned_data.get("where_taken")
+        date_accuracy = cleaned_data.get("date_accuracy")
+        name_is_the_same = cleaned_data.get("name_is_the_same")
+
+        if where_taken == self.WhereTaken.SAME_UNIT:
+            self.cleaned_data["provider"] = self.current_provider
+        elif where_taken == self.WhereTaken.ANOTHER_UNIT and not cleaned_data.get(
+            "provider"
+        ):
+            self.add_error(
+                "provider",
+                ValidationError(
+                    "Select another brest screening unit.", code="required"
+                ),
+            )
+        elif where_taken in (
+            self.WhereTaken.ELSEWHERE_UK,
+            self.WhereTaken.OUTSIDE_UK,
+        ) and not cleaned_data.get("location_details"):
+            self.add_error(
+                "location_details",
+                ValidationError(
+                    "Provide the clinic or hospital name, or any location details.",
+                    code="required",
+                ),
+            )
+
+        if date_accuracy == "exact" and not cleaned_data.get("exact_date"):
+            self.add_error(
+                "exact_date",
+                ValidationError("Provide the date.", code="required"),
+            )
+        elif date_accuracy == "approx" and not cleaned_data.get("approx_date"):
+            self.add_error(
+                "approx_date",
+                ValidationError("Provide the approximate date.", code="required"),
+            )
+
+        if name_is_the_same == "no" and not cleaned_data.get("different_name"):
+            self.add_error(
+                "different_name",
+                ValidationError("Provide the previous name.", code="required"),
+            )
+
+    def set_location_type(self, instance):
+        where_taken = self.cleaned_data["where_taken"]
+        if where_taken in (self.WhereTaken.SAME_UNIT, self.WhereTaken.ANOTHER_UNIT):
+            instance.location_type = (
+                ParticipantReportedMammogram.LocationType.NHS_BREAST_SCREENING_UNIT
+            )
+        else:
+            instance.location_type = where_taken
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+
+        instance.participant = self.participant
+        self.set_location_type(instance)
+
+        if commit:
+            instance.save()
+
+        return instance
